f96d5d6e0c148303668205c30898ec00542fe214
sanguchit0demiga <camilarocio005@gmail.com>
2024-09-01 19:59:53 -0300

    add tag and bool for jump

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class MoveObject : MonoBehaviour
 {
@@ -9,10 +10,16 @@ public class MoveObject : MonoBehaviour
     public float jumpforce;
     public Vector2 inputvector;
     public Rigidbody rigidBody;
+    public Vector3 velocity;
+    public float velocityMagnitude;
+    public bool CanJump;
+
     void Start()
     {
        rigidBody = GetComponent<Rigidbody>(); 
+        CanJump = true;
     }
+
 
     // Update is called once per frame
     void Update()
@@ -21,9 +28,18 @@ public class MoveObject : MonoBehaviour
         inputvector.y = Input.GetAxis("Vertical");
 
        rigidBody.AddForce(inputvector.x*speed,0f, inputvector.y*speed, ForceMode.Impulse);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && CanJump)
         {
             rigidBody.AddForce(0f, jumpforce,0f, ForceMode.Impulse);
+            CanJump = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("choque contra: " + collision.gameObject.tag);
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            CanJump = true;
         }
     }
 }
